b0b7df10eb0a88a96db26442bb5b9a5b431c7867
Yunfei Wang <Yunfei Wang>
2017-04-04 23:05:46 -0400

    API Key working, able to get objects including recipe id number

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,66 +29,74 @@ $('#getRecipe').on('click', function showResults(){
   event.preventDefault();
   var recipeResults = $(this).attr("data-results");
 
-  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms;
+  // var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms;
+  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms + "&limitLicense=false&number=5&ranking=1";
 
   $.ajax({
     url: queryURL,
     method: "GET",
     dataType: "JSON",
-    headers: {"X-Mashape-Key": "i4ECcdiynmmshKBDEhBL328ZNL7Xp1Q6tqXjsnJFxvS0ZxoSF1"}
+    headers: {"X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez"}
   })
     .done(function(response){
-    console.log("url:" + queryURL);
-    console.log("response:" + response);
-    var recipedata = response.data;
+      console.log(response);
+    var recipedata = response;
+    console.log(recipedata);
+
+    var image;
+    var title;
+    var likes;
+    var idNum;
 
     for (var i=0; i<recipedata.length; i++) {
       var resultRecipe = $("<div class='result'>");
-      var image = $("<img>").attr("src", recipedata[i].image);
-      var title = $("<p>").text("title: " + recipedata[i].title);
-      var likes = $("<p>").text("likes: " + recipedata[i].likes);
+      image = $("<img>").attr("src", recipedata[i].image);
+      title = $("<p>").text("title: " + recipedata[i].title);
+      likes = $("<p>").text("likes: " + recipedata[i].likes);
+      idNum = recipedata[i].id;
+      console.log(idNum);
     }
+    function findRecipe(numRecipes, queryRecipe) {
+
+        $.ajax({
+
+            url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + idNum + "/analyzedInstructions?stepBreakdown=true",
+            method: "GET",
+            dataType: "JSON",
+            headers: {
+            	"X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez"
+            }
+
+        }).done(function(recipes) {
+
+        	console.log("recipes:", recipes);
+
+        	// if (recipes.length = 0) {
+
+        	// 	console.log("error");
+
+        	// 	// **display error message for user in html
+        	// }
+
+         //    // Logging the URL for troubleshooting access
+         //    console.log("URL: " + queryRecipe);
+
+         //    // Logging recipes to console
+         //    console.log(recipes.length);
+
+         //    // Loop through and provide the correct number of recipes
+         //    for (var i = 0; i < recipes.length; i++) {
+
+         //    	// append to HTML section for recipes;
+         //    	var recipeSection = $("<div>");
+         //    	$("#unknown").append(recipes);
+        });
+    }
+
+    findRecipe();
+
   });
 });
-// $('#ingSubmit').on('click', function() {
-//     event.preventDefault();
-
-//     var userInp = $('#addItem').val().trim();
-
-//     var list = $('<ul>');
-//     list.append('<li>' + userInp + '</li>');
-//     $('.list').append(list);
-//     $('#addItem').val('');
-// });
-
-// // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyB9zqrDimq0-II0wdrXEIga34C-dTJwoV0",
-//     authDomain: "whats4dinner-eab75.firebaseapp.com",
-//     databaseURL: "https://whats4dinner-eab75.firebaseio.com",
-//     storageBucket: "whats4dinner-eab75.appspot.com",
-//     messagingSenderId: "1061192161195"
-// };
-// firebase.initializeApp(config);
-
-// var database = firebase.database();
-
-// // #addZip
-// // #searchZip
-// // #groceryStores
-
-// var zipCode;
-// var userurl;
-// $("#searchZip").on("click", function(event) {
-//     event.preventDefault();
-//     zipCode = $("#addZip").val().trim();
-//     userurl = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBGnB25L1jvt7LwgV8_YnEQoFx6SAcR048&q=grocery+stores+near+" + zipCode;
-
-//     $("iframe").attr("src", userurl);
-
-//     console.log("zipcode: " + zipCode);
-//     console.log("userurl: " + userurl);
-// });
 
 // Get the modal
 var modal = $('#myModal')[0];
@@ -97,9 +105,6 @@ var mapbtn = $("#openmap")[0];
 // Get the <span> element that closes the modal
 var span = $(".close")[0];
 // When the user clicks on the button, open the modal
-
-
-
 $("#openmap").on("click", function(event){
   event.preventDefault();
   modal.style.display = "block";
@@ -118,24 +123,17 @@ window.onclick = function(event) {
 }
 
 
-// #addZip
-// #searchZip
-// #groceryStores
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyB9zqrDimq0-II0wdrXEIga34C-dTJwoV0",
+//     authDomain: "whats4dinner-eab75.firebaseapp.com",
+//     databaseURL: "https://whats4dinner-eab75.firebaseio.com",
+//     storageBucket: "whats4dinner-eab75.appspot.com",
+//     messagingSenderId: "1061192161195"
+// };
+// firebase.initializeApp(config);
 
-// var zipCode;
-// var userurl;
-// $("#searchZip").on("click", function(event){
-//   event.preventDefault();
-//   zipCode = $("#addZip").val().trim();
-//   userurl = "https://www.google.com/maps/embed/v1/search?key=AIzaSyBGnB25L1jvt7LwgV8_YnEQoFx6SAcR048&q=grocery+stores+near+" + zipCode;
-//
-//   $("iframe").attr("src", userurl);
-//
-//   console.log("zipcode: " + zipCode);
-//   console.log("userurl: " + userurl);
-// });
-
-
+// var database = firebase.database();
 
 
 // var recent = "";
@@ -156,98 +154,3 @@ window.onclick = function(event) {
 // firebase.database().ref().orderByChild("dataAdded").limitToLast(1).on("value", function(snapshot){
 // 	$(".recent-list").html(snapshot.val().name);
 // });
-// var recent = "";
-// var favorite = "";
-
-// $("#recentBtn").on("click", function(event) {
-//     event.preventDefault();
-//     recent =
-//         firebase.database().push({
-//             recent: recent
-//         });
-
-//     firebase.database().ref().on("child_added", function(snapshot) {
-//         $(".recent-list").append("<div>" + snapshot.val().recent + "</div>");
-//     });
-// });
-
-// firebase.database().ref().orderByChild("dataAdded").limitToLast(1).on("value", function(snapshot) {
-//     $(".recent-list").html(snapshot.val().name);
-// });
-
-// $('#getRecipe').on('click', function() {
-//     event.preventDefault();
-
-// });
-
-// $.ajax({url: url, method: "GET"});
-// .done(function(response){
-//   var results = response.data;
-//   for (var i=0; i<results.length; i++) {
-//     var
-//     restults[i]
-//   }
-// })
-
-// var authKey;
-// var ingredients;
-// var queryRecipe = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + findByIngredients + "&limitLicense=false&number=10&ranking=1";
-
-
-// curl --get --include 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=1' \
-//   -H 'X-Mashape-Key: VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez' \
-//   -H 'Accept: application/json'
-
-function findRecipe(numRecipes, queryRecipe) {
-
-    $.ajax({
-    	
-        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/324694/analyzedInstructions?stepBreakdown=true",
-        method: "GET",
-        dataType: "JSON",
-        headers: {
-        	"X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez"
-        }
-
-
-
-    }).done(function(recipes) {
-
-    	console.log("recipes:", recipes);
-
-    	// if (recipes.length = 0) {
-
-    	// 	console.log("error");
-
-    	// 	// **display error message for user in html
-    	// } 
-
-     //    // Logging the URL for troubleshooting access
-     //    console.log("URL: " + queryRecipe);
-
-     //    // Logging recipes to console
-     //    console.log(recipes.length);
-
-     //    // Loop through and provide the correct number of recipes
-     //    for (var i = 0; i < recipes.length; i++) {
-
-
-
-        	
-
-     //    	// append to HTML section for recipes;
-     //    	var recipeSection = $("<div>");
-     //    	$("#unknown").append(recipes);
-
-
-
-
-
-
-    });
-
-
-
-}
-
-findRecipe();
