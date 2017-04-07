9a98042deeb25a21703abd856fc259216029e3d1
Priya Iyar <Priya Iyar>
2017-04-07 17:16:52 -0400

    map changes, fixed user input for zipcode/added input to modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
-
 // show reccommended recipes when page first loaded
-$(function(){
-  console.log("page loaded, show reccommended recipes in div #recipePreview");
+$(function() {
+    console.log("page loaded, show reccommended recipes in div #recipePreview");
 });
 
 var searchTerms = "";
@@ -9,12 +8,13 @@ var ingredientCode = 0
 $('#ingSubmit').on('click', function() {
     event.preventDefault();
     var userInp = $('#addItem').val().trim();
+    $("#addItem").val("");
 
     // add user input ingredients to searchTerms in queryURL
-    if (searchTerms==""){
-      searchTerms = userInp;
-    } else if (searchTerms!=""){
-      searchTerms = searchTerms + "%2C" + userInp; //"%2C" means ","
+    if (searchTerms == "") {
+        searchTerms = userInp;
+    } else if (searchTerms != "") {
+        searchTerms = searchTerms + "%2C" + userInp; //"%2C" means ","
     }
     console.log("searchTerms:" + searchTerms);
 
@@ -38,82 +38,83 @@ $('#ingSubmit').on('click', function() {
 
     // If user decides to remove item, ammend the list and string
     $('.ingredientButton').on('click', function(event) {
-      $(this).remove();
+        $(this).remove();
     });
 });
 
 // show search results of recipe preview: image, title, and likes
-$('#getRecipe').on('click', function showResults(){
-  event.preventDefault();
-  var recipeResults = $(this).attr("data-results");
+$('#getRecipe').on('click', function showResults() {
+    event.preventDefault();
+    var recipeResults = $(this).attr("data-results");
 
-  // var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms;
-  var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms + "&limitLicense=false&number=5&ranking=1";
+    // var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms;
+    var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + searchTerms + "&limitLicense=false&number=5&ranking=1";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    dataType: "JSON",
-    headers: {"X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez"}
-  })
-    .done(function(response){
-      console.log(response);
-    var recipedata = response;
-    console.log(recipedata);
-
-    var image;
-    var title;
-    var likes;
-    var idNum;
-
-    for (var i=0; i<recipedata.length; i++) {
-      var resultRecipe = $("<div class='result'>");
-      image = $("<img>").attr("src", recipedata[i].image);
-      title = $("<p>").text("title: " + recipedata[i].title);
-      likes = $("<p>").text("likes: " + recipedata[i].likes);
-      idNum = recipedata[i].id;
-      console.log(idNum);
-    }
-    function findRecipe(numRecipes, queryRecipe) {
-
-        $.ajax({
-
-            url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + idNum + "/analyzedInstructions?stepBreakdown=true",
+    $.ajax({
+            url: queryURL,
             method: "GET",
             dataType: "JSON",
-            headers: {
-              "X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez"
+            headers: { "X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez" }
+        })
+        .done(function(response) {
+            console.log(response);
+            var recipedata = response;
+            console.log(recipedata);
+
+            var image;
+            var title;
+            var likes;
+            var idNum;
+
+            for (var i = 0; i < recipedata.length; i++) {
+                var resultRecipe = $("<div class='result'>");
+                image = $("<img>").attr("src", recipedata[i].image);
+                title = $("<p>").text("title: " + recipedata[i].title);
+                likes = $("<p>").text("likes: " + recipedata[i].likes);
+                idNum = recipedata[i].id;
+                console.log(idNum);
             }
 
-        }).done(function(recipes) {
+            function findRecipe(numRecipes, queryRecipe) {
 
-          console.log("recipes:", recipes);
+                $.ajax({
 
-          // if (recipes.length = 0) {
+                    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + idNum + "/analyzedInstructions?stepBreakdown=true",
+                    method: "GET",
+                    dataType: "JSON",
+                    headers: {
+                        "X-Mashape-Key": "VS2DQ1Z8NsmshinFxHOYEzkSKA9Hp1dqzxFjsnBjVYMArEc4Ez"
+                    }
 
-          //  console.log("error");
+                }).done(function(recipes) {
 
-          //  // **display error message for user in html
-          // }
+                    console.log("recipes:", recipes);
 
-         //    // Logging the URL for troubleshooting access
-         //    console.log("URL: " + queryRecipe);
+                    // if (recipes.length = 0) {
 
-         //    // Logging recipes to console
-         //    console.log(recipes.length);
+                    //  console.log("error");
 
-         //    // Loop through and provide the correct number of recipes
-         //    for (var i = 0; i < recipes.length; i++) {
+                    //  // **display error message for user in html
+                    // }
 
-         //     // append to HTML section for recipes;
-         //     var recipeSection = $("<div>");
-         //     $("#unknown").append(recipes);
+                    //    // Logging the URL for troubleshooting access
+                    //    console.log("URL: " + queryRecipe);
+
+                    //    // Logging recipes to console
+                    //    console.log(recipes.length);
+
+                    //    // Loop through and provide the correct number of recipes
+                    //    for (var i = 0; i < recipes.length; i++) {
+
+                    //     // append to HTML section for recipes;
+                    //     var recipeSection = $("<div>");
+                    //     $("#unknown").append(recipes);
+                });
+            }
+
+            findRecipe();
+
         });
-    }
-
-    findRecipe();
-
-  });
 });
 
 // Get the modal
@@ -123,22 +124,28 @@ var mapbtn = $("#openmap")[0];
 // Get the <span> element that closes the modal
 var span = $(".close")[0];
 // When the user clicks on the button, open the modal
-$("#openmap").on("click", function(event){
-  event.preventDefault();
-  modal.style.display = "block";
-  zipCode = $("#addZip").val().trim();
-  userurl = "https:www.google.com/maps/embed/v1/search?key=AIzaSyBGnB25L1jvt7LwgV8_YnEQoFx6SAcR048&q=grocery+stores+near+" + zipCode;
+$("#openmap").on("click", function(event) {
+    event.preventDefault();
+    modal.style.display = "block";
+    
 });
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+$("#searchZip").on("click", function() {
+    zipCode = $("#addZip").val().trim();
+    userurl = "https:www.google.com/maps/embed/v1/search?key=AIzaSyBGnB25L1jvt7LwgV8_YnEQoFx6SAcR048&q=grocery+stores+near+" + zipCode;
+    $("#storeMap").attr("src", userurl);
+
+});
 
 
 // // Initialize Firebase
